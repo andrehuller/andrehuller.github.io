a9dc0115595a1899277a39cff2228a57d45378af
Andre Huller <andrehuller@gmail.com>
2020-10-04 11:54:57 -0300

    Separate JSONs

---
+++ src/libraries.js
@@ -0,0 +1,90 @@
+const libraries = [
+  {
+    "title": "ApexCharts.js",
+    "subtitle": "Modern & Interactive Open-source Charts",
+    "href": "https://apexcharts.com/",
+    "group": "Charts"
+  },
+  {
+    "title": "axios",
+    "subtitle": "Promise based HTTP client for the browser and node.js",
+    "href": "https://github.com/axios/axios",
+    "group": "Library"
+  },
+  {
+    "title": "Bootstrap",
+    "subtitle": "The most popular HTML, CSS, and JS library in the world",
+    "href": "https://getbootstrap.com/",
+    "group": "Library"
+  },
+  {
+    "title": "Chart.js",
+    "subtitle": "Simple yet flexible JavaScript charting for designers & developers",
+    "href": "https://www.chartjs.org/",
+    "group": "Charts"
+  },
+  {
+    "title": "D3.js",
+    "subtitle": "Data-Driven Documents",
+    "href": "https://d3js.org/",
+    "group": "Data"
+  },
+  {
+    "title": "DataTables",
+    "subtitle": "Table plug-in for jQuery",
+    "href": "https://datatables.net/",
+    "group": "Data"
+  },
+  {
+    "title": "jQuery",
+    "subtitle": "write less, do more.",
+    "href": "https://jquery.com/",
+    "group": "Frameworks"
+  },
+  {
+    "title": "Leaflet",
+    "subtitle": "mobile-friendly interactive maps",
+    "href": "https://leafletjs.com/",
+    "group": "Map"
+  },
+  {
+    "title": "Lodash",
+    "subtitle": "A modern JavaScript utility library delivering modularity, performance & extras",
+    "href": "https://lodash.com/",
+    "group": "Utilities",
+    "highlight": true
+  },
+  {
+    "title": "Moment",
+    "subtitle": "Parse, validate, manipulate, and display dates and times in JavaScript.",
+    "href": "https://momentjs.com/",
+    "group": "Date"
+  },
+  {
+    "title": "OpenLayers",
+    "subtitle": "A high-performance, feature-packed library for all your mapping needs",
+    "href": "https://openlayers.org/",
+    "group": "Map",
+    "highlight": true
+  },
+  {
+    "title": "Standard.js",
+    "subtitle": "JavaScript Standard Style",
+    "href": "https://standardjs.com/",
+    "group": "Formatting"
+  },
+  {
+    "title": "Vue",
+    "subtitle": "The Progressive JavaScript Framework",
+    "href": "https://vuejs.org",
+    "group": "Frameworks",
+    "highlight": true
+  },
+  {
+    "title": "Vuetify",
+    "subtitle": "Material Design Component Framework",
+    "href": "https://vuetifyjs.com/en/",
+    "group": "UI Components",
+    "highlight": true
+  }
+]
---
+++ src/locations.js
@@ -0,0 +1,128 @@
+const locations = [
+  {
+    "title": "Arc de Triomphe, Paris, France",
+    "country": "France",
+    "src": "assets/rodrigo-kugnharski-pdWc5wm1STw-unsplash.jpg",
+    "href": "https://unsplash.com/photos/pdWc5wm1STw"
+  },
+  {
+    "title": "Chichén Itzá, Mexico",
+    "country": "Mexico",
+    "src": "assets/filip-gielda-VPavA7BBxK0-unsplash.jpg",
+    "href": "https://unsplash.com/photos/VPavA7BBxK0"
+  },
+  {
+    "title": "Chureito Pagoda, Fujiyoshida-shi, Japan",
+    "country": "Japan",
+    "src": "assets/manuel-cosentino-n--CMLApjfI-unsplash.jpg",
+    "href": "https://unsplash.com/photos/n--CMLApjfI"
+  },
+  {
+    "title": "Colosseum, Roma, Italy",
+    "country": "Italy",
+    "src": "assets/henry-paul-on4xM3i9_FE-unsplash.jpg",
+    "href": "https://unsplash.com/photos/on4xM3i9_FE"
+  },
+  {
+    "title": "Easter Island, Chile",
+    "country": "Chile",
+    "src": "assets/thomas-griggs-2Qjk2PfaH3o-unsplash.jpg",
+    "href": "https://unsplash.com/photos/2Qjk2PfaH3o"
+  },
+  {
+    "title": "Eiffel Tower, Paris, France",
+    "country": "France",
+    "src": "assets/chris-karidis-nnzkZNYWHaU-unsplash.jpg",
+    "href": "https://unsplash.com/photos/nnzkZNYWHaU"
+  },
+  {
+    "title": "Great Wall of China, China",
+    "country": "China",
+    "src": "assets/hanson-lu-_8EFj6ISA08-unsplash.jpg",
+    "href": "https://unsplash.com/photos/_8EFj6ISA08"
+  },
+  {
+    "title": "Kremlin, Moscow, Russia",
+    "country": "Russia",
+    "src": "assets/michael-parulava-L4jrg4c7928-unsplash.jpg",
+    "href": "https://unsplash.com/photos/L4jrg4c7928"
+  },
+  {
+    "title": "London, United Kingdom",
+    "country": "United Kingdom",
+    "src": "assets/eva-dang-EXdXLrZXS9Q-unsplash.jpg",
+    "href": "https://unsplash.com/photos/EXdXLrZXS9Q"
+  },
+  {
+    "title": "Louvre, Paris, France",
+    "country": "France",
+    "src": "assets/sasan-hezarkhani--F-C5EeYa-s-unsplash.jpg",
+    "href": "https://unsplash.com/photos/-F-C5EeYa-s"
+  },
+  {
+    "title": "Machu Picchu, Peru",
+    "country": "Peru",
+    "src": "assets/willian-justen-de-vasconcellos-4hMET7vYTAQ-unsplash.jpg",
+    "href": "https://unsplash.com/photos/4hMET7vYTAQ"
+  },
+  {
+    "title": "Mount Rushmore, South Dakota, United States",
+    "country": "United States",
+    "src": "assets/stephen-F0T6QIPDrDc-unsplash.jpg",
+    "href": "https://unsplash.com/photos/F0T6QIPDrDc"
+  },
+  {
+    "title": "Notre Dame, Paris, France",
+    "country": "France",
+    "src": "assets/rita-chou-Qz8vpLWQZ6A-unsplash.jpg",
+    "href": "https://unsplash.com/photos/Qz8vpLWQZ6A"
+  },
+  {
+    "title": "Parthenon, Athens, Greece",
+    "country": "Greece",
+    "src": "assets/hans-reniers-DELDTYAjPrg-unsplash.jpg",
+    "href": "https://unsplash.com/photos/DELDTYAjPrg"
+  },
+  {
+    "title": "Petra, Jordan",
+    "country": "Jordan",
+    "src": "assets/ahmad-qaisieh-dnNSg7qxhV0-unsplash.jpg",
+    "href": "https://unsplash.com/photos/dnNSg7qxhV0"
+  },
+  {
+    "title": "Porto, Portugal",
+    "country": "Portugal",
+    "src": "assets/nick-karvounis-Prb-sjOUBFs-unsplash.jpg",
+    "href": "https://unsplash.com/photos/Prb-sjOUBFs"
+  },
+  {
+    "title": "Rio de Janeiro, Brazil",
+    "country": "Brazil",
+    "src": "assets/agustin-diaz-7F65HDP0-E0-unsplash.jpg",
+    "href": "https://unsplash.com/photos/7F65HDP0-E0"
+  },
+  {
+    "title": "Rothenburg ob der Tauber, Germany",
+    "country": "Germany",
+    "src": "assets/roman-kraft-g_gwdpsCVAY-unsplash.jpg",
+    "href": "https://unsplash.com/photos/g_gwdpsCVAY"
+  },
+  {
+    "title": "Taj Mahal, Agra, India",
+    "country": "India",
+    "src": "assets/julian-yu-_WuPjE-MPHo-unsplash.jpg",
+    "href": "https://unsplash.com/photos/_WuPjE-MPHo"
+  },
+  {
+    "title": "The Theban Necropolis, Al Aqaleta, Egypt",
+    "country": "Egypt",
+    "src": "assets/mo-gabrail-iuC3w8mLDcs-unsplash.jpg",
+    "href": "https://unsplash.com/photos/iuC3w8mLDcs"
+  },
+  {
+    "title": "Trevi Fountain, Roma, Italy",
+    "country": "Italy",
+    "src": "assets/cristina-gottardi-I1Lv2yX67GI-unsplash.jpg",
+    "href": "https://unsplash.com/photos/I1Lv2yX67GI"
+  }
+]
