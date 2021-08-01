24593ca9066550cc5ab7b3a2559705ca5329ff39
Andre Huller <andrehuller@gmail.com>
2021-07-31 21:26:38 -0300

    SVG Maps

--- src/components/Leaflet.js
+++ src/components/Leaflet.js
@@ -4,7 +4,7 @@ const Leaflet = {
       <v-row>
         <v-col lg="12">
           <v-card outlined>
-            <div id="map">
+            <div id="map" style="height: 855px"></div>
           </v-card>
         </v-col>
       
@@ -85,7 +85,7 @@ const Leaflet = {
   },
   mounted () {
     this.$nextTick(() => {
-      this.map = L.map('map').setView([-24.618588, -51.316993], 7);
+      this.map = L.map('map').setView([-24.618588, -51.316993], 8);
       
       L.control.defaultExtent()
         .addTo(this.map);
@@ -105,7 +105,8 @@ const Leaflet = {
   },
   methods: {
     init () {
-      var style = function (feature) {
+      /*
+      var style = function (feature) {       
         var colors = {
           4108: '#67001f',
           4102: '#b2182b',
@@ -127,22 +128,88 @@ const Leaflet = {
           fillOpacity: 0.7
         }
       }
+      */
+      // itcg:municipios_pol_p31982_e50
+      // ipardes:mesorregiao_pol_p4674_a2015
+      var myStyle = {
+        "color": "#0570b0",
+      };
 
+      var geojson = L.geoJson(null, {
+        style: myStyle
+      }).addTo(this.map)
+      
+      geojson.addData(r297484)
+      geojson.addData(r297485)
+      geojson.addData(r297486)
+      geojson.addData(r297487)
+      geojson.addData(r297488)
+      geojson.addData(r297489)
+      geojson.addData(r297490)
+      geojson.addData(r297491)
+      geojson.addData(r297492)
+      geojson.addData(r297493)
+      geojson.addData(r297494)
+      geojson.addData(r297495)
+      geojson.addData(r297496)
+      geojson.addData(r297497)
+      geojson.addData(r297498)
+      geojson.addData(r297499)
+      geojson.addData(r297500)
+      geojson.addData(r297501)
+      geojson.addData(r297502)
+      geojson.addData(r297503)
+      geojson.addData(r297504)
+      geojson.addData(r297505)
+      geojson.addData(r297506)
+      geojson.addData(r297507)
+      geojson.addData(r297508)
+      geojson.addData(r297509)
+      geojson.addData(r297510)
+      geojson.addData(r297511)
+      geojson.addData(r297512)
+      geojson.addData(r297513)
+      geojson.addData(r297514)
+      geojson.addData(r297515)
+      geojson.addData(r297516)
+      geojson.addData(r297517)
+      geojson.addData(r297518)
+      geojson.addData(r297519)
+      geojson.addData(r297520)
+
+      geojson.addData(r297533)
+
+      geojson.addData(r297610)
+      geojson.addData(r297611)
+
+      geojson.addData(r297651)
+
+      geojson.addData(r297719)
+
+      geojson.addData(r297756)
+
+      geojson.addData(r297796)
+
+      geojson.addData(r297846)
+      
+      /*
       axios.get('https://geoserver.pr.gov.br/geoserver/wfs', {
         params: {
           service: 'wfs',
           version: '2.0.0',
           outputFormat: 'application/json',
           request: 'GetFeature',
-          typeNames: 'ipardes:mesorregiao_pol_p4674_a2015'
+          typeNames: 'itcg:municipios_pr_pol_p29192_e1000_a2013',
+          srsName: 'EPSG:4674'
         }
       })
         .then(response => {
           // store.commit('setMesorregiao', response.data.features)
           var geojson = L.geoJson(response.data, {
-            style: style,
+            // style: style,
             onEachFeature: (feature, layer) => {
-              layer.bindPopup(L.Util.template('Nome: {nome_regiao}<br>Código: {cod_regiao}', feature.properties))
+              // layer.bindPopup(L.Util.template('Nome: {nome_regiao}<br>Código: {cod_regiao}', feature.properties))
+              console.log('!')
             }
           }).addTo(this.map)
 
@@ -151,6 +218,7 @@ const Leaflet = {
         .catch(error => {
           console.log(error)
         })
+      */
     }
   }
 }
