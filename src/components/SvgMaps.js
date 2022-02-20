5cb04dd9c7c4b48767c5e1c4a6e53f096bf785bf
Andre Huller <andrehuller@gmail.com>
2022-02-19 21:57:11 -0300

    Remove cities

--- src/components/SvgMaps.js
+++ src/components/SvgMaps.js
@@ -1,48 +1,19 @@
 const SvgMaps = {
   template: `
-    <v-container fluid pa-0>
-      <v-tabs grow>
-        <v-tab>
-          States
-        </v-tab>
-        <v-tab-item class="grey lighten-5">
-          <v-container fluid>
-            <v-row justify="center">
-              <v-col cols="12" lg="6">
-                <svg-map
-                  :map="states"
-                ></svg-map>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-tab-item>
-        <v-tab>
-          Cities
-        </v-tab>
-        <v-tab-item class="grey lighten-5">
-          <v-row justify="center">
-            <v-col cols="12" lg="9">
-              <svg-map
-                :map="cities"
-                :location-class="getLocationClass"
-              ></svg-map>
-            </v-col>
-          </v-row>
-        </v-tab-item>
-      </v-tabs>
+    <v-container fluid>
+      <v-row justify="center">
+        <v-col cols="12" lg="6">
+          <svg-map
+            :map="states"
+          ></svg-map>
+        </v-col>
+      </v-row>
     </v-container>
   `,
   data: () => ({
-    states: states,
-    cities: cities
+    states: states
   }),
   components: {
     "svg-map": VueSvgMap.SvgMap
-  },
-  methods: {
-    getLocationClass (location, index) {
-			// Generate heat map
-			return `svg-map__location svg-map__location--heat${index % 4}`
-		}
   }
 }
