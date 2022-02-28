97c8abcecbe8fb56625353ab154954113483ee3f
Andre Huller <andrehuller@gmail.com>
2022-02-28 11:10:40 -0300

    Add frameworks

--- src/components/Framework.js
+++ src/components/Framework.js
@@ -7,9 +7,13 @@ const Frameworks = {
             <v-timeline-item
               v-for="framework in frameworks"
               :key="framework.name"
-              color="#25305E"
             >
               <v-card>
+                <v-img
+                  v-if="framework.src"
+                  :src="framework.src"
+                  height="180px"
+                ></v-img>
                 <v-card-title>{{ framework.name }}</v-card-title>
                 <v-card-text v-if="framework.release">
                   {{ framework.release }}
@@ -25,35 +29,43 @@ const Frameworks = {
     frameworks: [
       {
         "name": "Vue.js",
-        "release": "02/2014"
+        "release": "02/2014",
+        "src": "assets/frameworks/vue.png"
       },
       {
         "name": "React",
-        "release": "29/05/2013"
+        "release": "29/05/2013",
+        "src": "assets/frameworks/react.png"
       },
       {
         "name": "Ember.js",
-        "release": "08/12/2011"
+        "release": "08/12/2011",
+        "src": "assets/frameworks/ember.png"
       },
       {
         "name": "AngularJS",
-        "release": "20/10/2010"
+        "release": "20/10/2010",
+        "src": "assets/frameworks/angular.png"
       },
       {
         "name": "Backbone.js",
-        "release": "13/10/2010"
+        "release": "13/10/2010",
+        "src": "assets/frameworks/backbone.png"
       },
       {
         "name": "Knockout.js",
-        "release": "05/07/2010"
+        "release": "05/07/2010",
+        "src": "assets/frameworks/knockout.png"
       },
       {
         "name": "jQuery",
-        "release": "01/2006"
+        "release": "01/2006",
+        "src": "assets/frameworks/jquery.png"
       },
       {
         "name": "Prototype",
-        "release": "02/2005"
+        "release": "02/2005",
+        "src": "assets/frameworks/prototype.png"
       }
     ]
   })
