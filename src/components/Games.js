1382c7e6f209f5debdc93b8168d69ff35f0b86dd
Andre Huller <andrehuller@gmail.com>
2022-06-23 13:38:51 -0300

    Add movies

--- src/components/Games.js
+++ src/components/Games.js
@@ -77,55 +77,55 @@ const Games = {
     games: [
       {
         "title": "Donkey Kong Country",
-        "src": "assets/images/Donkey Kong Country.jpg",
+        "src": "assets/games/Donkey Kong Country.jpg",
         "rating": 4.5,
         "difficulty": 3
       },
       {
         "title": "Donkey Kong Country 2", // Diddy's Kong Quest - Bonus Content: The Lost World
-        "src": "assets/images/Donkey Kong Country 2: Diddy's Kong Quest.png",
+        "src": "assets/games/Donkey Kong Country 2: Diddy's Kong Quest.png",
         "rating": 4.5,
         "difficulty": 3.5
       },
       {
         "title": "Super Ghouls 'n Ghosts",
-        "src": "assets/images/Super Ghouls 'n Ghosts.jpg",
+        "src": "assets/games/Super Ghouls 'n Ghosts.jpg",
         "rating": 4.5,
         "difficulty": 4
       },
       {
         "title": "Hagane",
-        "src": "assets/images/Hagane.jpg",
+        "src": "assets/games/Hagane.jpg",
         "rating": 4.5,
         "difficulty": 4
       },
       {
         "title": "Maui Mallard in Cold Shadow", // Prefer (U) version
-        "src": "assets/images/Maui Mallard in Cold Shadow.jpg",
+        "src": "assets/games/Maui Mallard in Cold Shadow.jpg",
         "rating": 4,
         "difficulty": 3
       },
       {
         "title": "Animaniacs",
-        "src": "assets/images/Animaniacs.png",
+        "src": "assets/games/Animaniacs.png",
         "rating": 4,
         "difficulty": 4
       },
       {
         "title": "Super Castlevania IV",
-        "src": "assets/images/Super Castlevania IV.jpg",
+        "src": "assets/games/Super Castlevania IV.jpg",
         "rating": 3.5,
         "difficulty": 2.5
       },
       {
         "title": "The Mask", // Really Tough Bosses
-        "src": "assets/images/The Mask.jpg",
+        "src": "assets/games/The Mask.jpg",
         "rating": 3,
         "difficulty": 3.5
       },
       {
         "title": "The Lion King", // Played in Normal
-        "src": "assets/images/The Lion King.jpg",
+        "src": "assets/games/The Lion King.jpg",
         "rating": 2,
         "difficulty": 4
       }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,83 +1,210 @@
 const Movies = {
   template: `
     <v-container fluid>
-      <!--
-      <v-row>
-        <v-col cols="12" sm="12" md="6" lg="2">
-          <v-card>
-            <v-img :src="image"></v-img>
-            <v-card-title>
-              The Haunting (1963)
-            </v-card-title>
-          </v-card>
-        </v-col>
-      </v-row>
-      -->
-      <v-row>
-        <v-col
-          v-for="item in items"
-          :key="item.title"
-          cols="12" sm="12" md="6" lg="3"
-        >
-          <v-card>
-            <v-img :src="'assets/images/movies/' + item.title + '.jpg'"></v-img>
-            <v-card-title>
-              {{ item.title }}
-            </v-card-title>
-            <v-card-subtitle>
-              {{ item.director }}
-            </v-card-subtitle>
-          </v-card>
-        </v-col>
-      </v-row>
+      <v-data-iterator
+        :items="items"
+        items-per-page="40"
+        :search="search"
+      >
+        <template v-slot:header>
+          <v-toolbar
+            dark
+            color="grey darken-3"
+            class="mb-3"
+          >
+            <v-container fluid>
+              <v-row>
+                <v-col lg="4">
+                  <v-text-field
+                    v-model="search"
+                    clearable
+                    flat
+                    solo-inverted
+                    hide-details
+                    prepend-inner-icon="mdi-magnify"
+                    label="Search"
+                  ></v-text-field>
+                </v-col>
+                <v-col lg="4">
+                  <v-autocomplete
+                    label="Director"
+                    :items="directors"
+                    hide-details
+                    clearable
+                    flat solo-inverted
+                    prepend-inner-icon="mdi-magnify"
+                  ></v-autocomplete>
+                </v-col>
+                <v-col lg="4">
+                  <v-select
+                    clearable
+                    flat solo-inverted
+                    hide-details
+                  ></v-select>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-toolbar>
+        </template>
+        <template v-slot:default="props">
+          <v-row>
+            <v-col
+              v-for="item in props.items"
+              :key="item.title"
+              cols="12" sm="12" md="6" lg="3"
+            >
+              <v-card>
+                <v-img
+                  :src="'assets/images/' + item.title + '.jpg'"
+                  height="198px"
+                ></v-img>
+                <v-card-subtitle>
+                  <b>{{ item.title }}</b>
+                </v-card-subtitle>
+                <v-card-text>
+                  {{ item.director }} / {{ item.year }}
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </template>
+      </v-data-iterator>
     </v-container>
   `,
   data: () => ({
-    image: 'assets/images/The Haunting (1963).jpg',
     items: [
       {
-        "title": "The Gold Rush (1925)",
-        "director": "Charles Chaplin"
+        "title": "The Gold Rush",
+        "director": "Charles Chaplin",
+        "year": 1925
       },
       {
-        "title": "To Be or Not to Be (1942)",
-        "director": "Ernst Lubitsch"
+        "title": "The General",
+        "director": "Buster Keaton",
+        "year": 1926
       },
       {
-        "title": "Sansho the Bailiff (1954)",
-        "director": "Kenji Mizoguchi"
+        "title": "Metropolis",
+        "director": "Fritz Lang",
+        "year": 1927
       },
       {
-        "title": "A Man Escaped (1956)",
-        "director": "Robert Bresson"
+        "title": "Casablanca",
+        "director": "Michael Curtiz",
+        "year": 1942
       },
       {
-        "title": "The Seventh Seal (1957)",
-        "director": "Ingmar Bergman"
+        "title": "To Be or Not to Be",
+        "director": "Ernst Lubitsch",
+        "year": 1942
       },
       {
-        "title": "Pickpocket (1959)",
-        "director": "Robert Bresson"
+        "title": "Sunset Blvd.",
+        "director": "Billy Wilder",
+        "year": 1950
       },
       {
-        "title": "8½ (1963)",
-        "director": "Federico Fellini"
+        "title": "Sansho the Bailiff",
+        "director": "Kenji Mizoguchi",
+        "year": 1954
       },
       {
-        "title": "Blow-Up (1966)",
-        "director": "Michelangelo Antonioni"
+        "title": "A Man Escaped",
+        "director": "Robert Bresson",
+        "year": 1956
       },
       {
-        "title": "A Clockwork Orange (1971)",
+        "title": "The Seventh Seal",
+        "director": "Ingmar Bergman",
+        "year": 1957
+      },
+      {
+        "title": "Wild Strawberries",
+        "director": "Ingmar Bergman",
+        "year": 1957
+      },
+      {
+        "title": "Touch of Evil",
+        "director": "Orson Welles",
+        "year": 1958
+      },
+      {
+        "title": "Pickpocket",
+        "director": "Robert Bresson",
+        "year": 1959
+      },
+      {
+        "title": "L'Avventura",
+        "director": "Michelangelo Antonioni",
+        "year": 1960
+      },
+      {
+        "title": "Viridiana",
+        "director": "Luis Buñuel",
+        "year": 1961
+      },
+      {
+        "title": "Lawrence of Arabia",
+        "director": "David Lean",
+        "year": 1962
+      },
+      {
+        "title": "8½",
+        "director": "Federico Fellini",
+        "year": 1963
+      },
+      {
+        "title": "Au hasard Balthazar",
+        "director": "Robert Bresson",
+        "year": 1966
+      },
+      {
+        "title": "Blow-Up",
+        "director": "Michelangelo Antonioni",
+        "year": 1966
+      },
+      {
+        "title": "Once Upon a Time in the West",
+        "director": "Sergio Leone",
+        "year": 1968
+      },
+      {
+        "title": "A Clockwork Orange",
+        "director": "Stanley Kubrick",
+        "year": 1971
+      },
+      {
+        "title": "Amarcord",
+        "director": "Federico Fellini",
+        "year": 1973
+      },
+      {
+        "title": "Barry Lyndon (1975)",
         "director": "Stanley Kubrick"
       },
       {
-        "title": "Annie Hall (1977)",
-        "director": "Woody Allen"
+        "title": "Annie Hall",
+        "director": "Woody Allen",
+        "year": 1977
+      },
+      {
+        "title": "Blade Runner",
+        "director": "Ridley Scott",
+        "year": 1982
       },
       {
         "title": "My Neighbor Totoro (1988)",
         "director": "Hayao Miyazaki"
+      },
+      {
+        "title": "Goodfellas",
+        "director": "Martin Scorsese",
+        "year": 1990
+      },
+      {
+        "title": "Pulp Fiction",
+        "director": "Quentin Tarantino",
+        "year": 1994
       },
       {
         "title": "Yi yi (2000)",
@@ -92,13 +219,25 @@ const Movies = {
         "director": "Hayao Miyazaki"
       },
       {
-        "title": "Eternal Sunshine of the Spotless Mind (2004)",
-        "director": "Michel Gondry"
+        "title": "Eternal Sunshine of the Spotless Mind",
+        "director": "Michel Gondry",
+        "year": 2004
       },
       {
         "title": "There Will Be Blood (2007)",
         "director": "Paul Thomas Anderson"
       }
-    ]
-  })
+    ],
+    directors: [],
+    search: null
+  }),
+  mounted: function () {
+    // this.directors = []
+    for (var i = 0; i < this.items.length; i++) {
+      if (this.directors.indexOf(this.items[i].director) < 0) {
+        this.directors.push(this.items[i].director)
+      }
+    }
+    this.directors = this.directors.sort()
+  }
 }
