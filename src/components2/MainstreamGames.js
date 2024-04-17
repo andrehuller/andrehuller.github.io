279d4bdade9e9f06287c755a4b370750a2b2cf48
ahuller <ahuller@celeparsl.smb4.parana>
2024-04-17 09:00:53 -0300

    Add games

---
+++ src/components2/MainstreamGames.js
@@ -0,0 +1,365 @@
+const MainstreamGames = {
+  template: `
+    <v-container fluid>
+      <v-row>
+        <v-col lg="3">
+          <v-card>
+            <v-img src="assets/gamesps4/Resident Evil.jpg" height="176.55" cover></v-img>
+          </v-card>
+        </v-col>
+        <v-col lg="3">
+          <v-card>
+            <v-img src="assets/gamesps4/Resident Evil 0.jpg" height="176.55" cover></v-img>
+          </v-card>
+        </v-col>
+        <v-col lg="3">
+          <v-card>
+            <v-img src="assets/gamesps4/Resident Evil 4 (2005).jpg" height="176.55" cover></v-img>
+          </v-card>
+        </v-col>
+
+        <v-col lg="3">
+          <v-card>
+            <v-img src="assets/gamesps4/Resident Evil 2.jpg" height="176.55" cover></v-img>
+          </v-card>
+        </v-col>
+        <v-col lg="3">
+          <v-card>
+            <v-img src="assets/gamesps4/Resident Evil 3.jpg" height="176.55" cover></v-img>
+          </v-card>
+        </v-col>
+        <v-col lg="3">
+          <v-card>
+            <v-img src="assets/gamesps4/Resident Evil 4 (2023).jpg" height="176.55" cover></v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col lg="3">
+          <v-card>
+            <v-img src="assets/gamesps4/Saints Row 2.jpg" height="176.55" cover></v-img>
+          </v-card>
+        </v-col>
+        <!-- Saints Row: The Third -->
+        <v-col lg="3">
+          <v-card>
+            <v-img src="assets/gamesps4/Saints Row The Third.jpg" height="176.55" cover></v-img>
+          </v-card>
+        </v-col>
+        <!-- Saints Row IV: Re-Elected -->
+        <v-col lg="3">
+          <v-card>
+            <v-img src="assets/gamesps4/Saints Row IV Re-Elected.jpg" height="176.55" cover></v-img>
+          </v-card>
+        </v-col>
+        <!-- Saints Row: Gat Out of Hell -->
+        <v-col lg="3">
+          <v-card>
+            <v-img src="assets/gamesps4/Saints Row Gat Out of Hell.jpg" height="176.55" cover></v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col lg="9">
+          <v-card color="blue-grey-lighten-4">
+            <v-card-title>Rocksteady Studios</v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col lg="4">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/Batman Arkham Asylum.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>Batman: Arkham Asylum (2010)</b></v-card-text>
+                  </v-card>
+                </v-col>
+                <v-col lg="4">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/Batman Arkham City.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>Batman: Arkham City (2012)</b></v-card-text>
+                  </v-card>
+                </v-col>
+                <v-col lg="4">
+                  <v-card color="blue-grey-lighten-5">
+                    <!-- Batman: Arkham Knight -->
+                    <v-img src="assets/gamesps4/Batman Arkham Knight.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>Batman: Arkham Knight (2015)</b></v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col lg="3">
+          <v-card color="blue-grey-lighten-4">
+            <v-card-title>Naughty Dog LLC</v-card-title>
+            <v-card-text>
+              <v-row>
+                <!-- The Last of Us™ Part I -->
+                <v-col lg="12">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/The Last of Us.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>The Last of Us (2013)</b></v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col lg="6">
+          <v-card color="blue-grey-lighten-4">
+            <v-card-title>id Software</v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col lg="6">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/Doom.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>DOOM (2016)</b></v-card-text>
+                  </v-card>
+                </v-col>
+                <v-col lg="6">
+                  <v-card color="blue-grey-darken-1">
+                    <v-img src="assets/gamesps4/Doom Eternal.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>DOOM Eternal (2020)</b></v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col lg="3">
+          <v-card color="blue-grey-lighten-4">
+            <v-card-title>Respawn Entertainment</v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col lg="12">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/Titanfall 2.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>Titanfall 2 (2016)</b></v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col lg="3">
+          <v-card color="blue-grey-lighten-4">
+            <v-card-title>MachineGames</v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col lg="12">
+                  <v-card color="blue-grey-darken-1">
+                    <v-img src="assets/gamesps4/Wolfenstein The New Order.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>Wolfenstein: The New Order (2014)</b></v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col lg="6">
+          <v-card color="blue-grey-lighten-4">
+            <v-card-title>NetherRealm Studios</v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col lg="6">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/Injustice Gods Among Us.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>Injustice: Gods Among Us (2013)</b></v-card-text>
+                  </v-card>
+                </v-col>
+                <v-col lg="6">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/Injustice 2.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>Injustice 2 (2017)</b></v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      
+        <v-col lg="6">
+          <v-card color="blue-grey-lighten-4">
+            <v-card-title>Guerrilla</v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col lg="6">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/Horizon Zero Dawn.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>Horizon Zero Dawn™ Complete Edition (2020)</b></v-card-text>
+                  </v-card>
+                </v-col>
+                <v-col lg="6">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/Horizon Forbidden West.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>Horizon Forbidden West™ Complete Edition (2024)</b></v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col lg="3">
+          <v-card color="blue-grey-lighten-4">
+            <v-card-title>Square Enix</v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col lg="12">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/NieR Automata.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>NieR:Automata (2017)</b></v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col lg="9">
+          <v-card color="blue-grey-lighten-4">
+            <v-card-title>Crystal Dynamics</v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col lg="4">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/Tomb Raider.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>Tomb Raider (2013)</b></v-card-text>
+                  </v-card>
+                </v-col>
+                <v-col lg="4">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/Rise of the Tomb Raider.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>Rise of the Tomb Raider (2016)</b></v-card-text>
+                  </v-card>
+                </v-col>
+                <v-col lg="4">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/Shadow of the Tomb Raider.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>Shadow of the Tomb Raider (2018)</b></v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+
+
+      <v-row>
+        <v-col lg="6">
+          <v-card color="blue-grey-lighten-4">
+            <v-card-title>2K</v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col lg="6">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/BioShock.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>BioShock (2007)</b></v-card-text>
+                  </v-card>
+                </v-col>
+
+                <v-col lg="6">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/BioShock Infinite.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>BioShock Infinite (2013)</b></v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col lg="6">
+          <v-card color="blue-grey-lighten-4">
+            <v-card-title>FromSoftware Inc.</v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col lg="6">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/Elden Ring.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>ELDEN RING (2022)</b></v-card-text>
+                  </v-card>
+                </v-col>
+                <v-col lg="6">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/Armored Core VI.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>ARMORED CORE™ VI FIRES OF RUBICON™ (2023)</b></v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <v-row>
+
+        <v-col lg="3">
+          <v-card color="blue-grey-lighten-4">
+            <v-card-title>CAPCOM Co., Ltd.</v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col lg="12">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/Dragon's Dogma 2.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>Dragon's Dogma 2 (2024)</b></v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col lg="6">
+          <v-card color="blue-grey-lighten-4">
+            <v-card-title>Larian Studios</v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col lg="6">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/Baldur's Gate 3.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>Baldur's Gate 3 (2023)</b></v-card-text>
+                  </v-card>
+                </v-col>
+                <v-col lg="6">
+                  <v-card color="blue-grey-lighten-5">
+                    <v-img src="assets/gamesps4/Divinity.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>Divinity: Original Sin - Enhanced Edition (2015)</b></v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col lg="3">
+          <v-card>
+            <v-card-title>Rockstar North</v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col lg="12">
+                  <v-card>
+                    <v-img src="assets/gamesps4/Grand Theft Auto V.jpg" height="176.55" cover></v-img>
+                    <v-card-text><b>Grand Theft Auto V (2015)</b></v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+      </v-row>
+    </v-container>
+  `
+}
