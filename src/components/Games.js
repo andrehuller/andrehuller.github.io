2bf0a725efa67dfd0e460cb62d61c01fe41a5236
Andre Huller <andrehuller@gmail.com>
2025-05-21 10:10:06 -0300

    Update index.html

---
+++ src/components/Games.js
@@ -0,0 +1,35 @@
+const Games = {
+  template: `
+    <v-container fluid>
+      <v-row>
+	<!-- https://eagleislandgame.com/press -->
+        <v-col lg="3">
+          <v-card class="fill-height d-flex flex-column">
+            <v-img src="assets/games/Eagle Island Twist.jpg" height="190" cover></v-img>
+            <v-card-text><b>Eagle Island Twist (2019)</b></v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col lg="3">
+          <v-card class="fill-height d-flex flex-column">
+            <v-img src="assets/games/Soulstice.jpg" height="190" cover></v-img>
+            <v-card-text><b>Soulstice (2022)</b></v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <!-- https://simonquest.com/press -->
+        <v-col lg="4">
+          <v-card class="fill-height d-flex flex-column">
+            <v-img src="assets/games/The Transylvania Adventure of Simon Quest.jpg"  cover></v-img>
+            <v-card-text><b>The Transylvania Adventure of Simon Quest (2025)</b></v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <!-- Inglorious Rex creator Shane Davis, Godlike and Graveyard Shift creator Jon Malin, and Cyberfrog creator Ethan Van Sciver -->
+      <!-- https://comicsgate.org/ -->
+    </v-container>
+  `
+}
