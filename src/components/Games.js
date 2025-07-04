6366ab1311d429a15bcbe6a120a561bfc5219b56
Andre Huller <andrehuller@gmail.com>
2025-07-04 10:01:59 -0300

    Add images

--- src/components/Games.js
+++ src/components/Games.js
@@ -137,36 +137,47 @@ const Games = {
       <v-row>
         <!-- https://www.igdb.com/games/hollow-knight-silksong/presskit -->
         <v-col lg="3">
-          <v-card class="fill-height d-flex flex-column" title="Hollow Knight: Silksong (2025)">
+          <v-card class="fill-height d-flex flex-column">
             <v-img src="assets/games/Hollow Knight Silksong.jpg" height="190" cover></v-img>
+            <v-card-item>
+              <v-card-title>Hollow Knight: Silksong (2025)</v-card-title>
+            </v-card-item>
           </v-card>
         </v-col>
 
         <!-- https://www.playsidestudios.com/ -->
         <v-col lg="3">
-          <v-card class="fill-height d-flex flex-column" title="Mouse PI (2025)">
+          <v-card class="fill-height d-flex flex-column">
             <v-img src="assets/games/Mouse PI.jpg" height="190" cover></v-img>
+            <v-card-item>
+              <v-card-title>Mouse PI (2025)</v-card-title>
+            </v-card-item>
           </v-card>
         </v-col>
 
 	<!-- https://impress.games/press-kit/zenovia-interactive/neon-inferno -->
         <v-col lg="3">
-          <v-card class="fill-height d-flex flex-column" title="Neon Inferno (2025)">
-            <template v-slot:append>
-              <v-chip>October 2nd</v-chip>
-            </template>
+          <v-card class="fill-height d-flex flex-column">
             <v-img src="assets/games/Neon Inferno.png" height="190" cover></v-img>
+            <v-card-item>
+              <v-card-title>Neon Inferno (2025)</v-card-title>
+              <template v-slot:append>
+                <v-chip>October 2nd</v-chip>
+              </template>
+            </v-card-item>
           </v-card>
         </v-col>
 
         <!-- https://playtoxiccrusaders.com/press -->
         <v-col lg="3">
-          <v-card class="fill-height d-flex flex-column" title="Toxic Crusaders">
-            <template v-slot:append>
-              <v-chip>Q3 2025</v-chip>
-            </template>
-
+          <v-card class="fill-height d-flex flex-column">
             <v-img src="assets/games/Toxic Crusaders.png" height="190" cover></v-img>
+            <v-card-item>
+              <v-card-title>Toxic Crusaders</v-card-title>
+              <template v-slot:append>
+                <v-chip>Q3 2025</v-chip>
+              </template>
+            </v-card-item>
           </v-card>
         </v-col>
       </v-row>
@@ -340,9 +351,14 @@ const Games = {
         <v-col lg="4">
           <v-card class="fill-height d-flex flex-column">
             <v-img src="assets/games/Terminator 2D.png"  cover></v-img>
-            <v-card-text><b>Terminator 2D: NO FATE (2025)</b></v-card-text>
+            <v-card-text >
+              <b>Terminator 2D: NO FATE (2025)</b>
+              September 5, 2025
+            </v-card-text>
+
           </v-card>
         </v-col>
+
       </v-row>
     </v-container>
   `
